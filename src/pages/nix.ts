f7e2cfd63ea10e47a61e57ea8843b323ed79596f
jaycem <45575946+jaycem-dev@users.noreply.github.com>
2026-02-16 15:31:34 -0300

    feat: nix install script redirect page

---
+++ src/pages/nix.ts
@@ -0,0 +1,13 @@
+export const GET = async () => {
+    const res = await fetch(
+        "https://raw.githubusercontent.com/jaycem-dev/nix/master/install.sh",
+    );
+    return new Response(await res.text(), {
+        headers: {
+            "Content-Type": "text/x-shellscript",
+            "Cache-Control": "no-cache, no-store, must-revalidate",
+            Pragma: "no-cache",
+            Expires: "0",
+        },
+    });
+};
